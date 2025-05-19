5b9f8d59e6fea6232923252849fdad7229b90399
Vadim Em <vadim@Vadims-MacBook-Air.local>
2025-05-19 18:01:00 +0500

    project init

---
+++ YandexMapKit/YandexMapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YandexMapKit
+//
+//  Created by Vadim Em on 19.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
