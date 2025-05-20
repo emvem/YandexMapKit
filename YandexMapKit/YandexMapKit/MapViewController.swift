0901fd483e8d73526e3da28af81b80bec9ac500c
Vadim Em <mr.vadim134@gmail.com>
2025-05-20 11:28:01 +0500

    default coordinates added

--- YandexMapKit/YandexMapKit/MapViewController.swift
+++ YandexMapKit/YandexMapKit/MapViewController.swift
@@ -16,12 +16,29 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
-        
+        setupViews()
+        setDefaultLocation()
+    }
+    
+    private func setDefaultLocation() {
+        let tashkent = YMKPoint(latitude: 41.3111, longitude: 69.2797)
+        move(to: tashkent)
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+
         view.addSubview(mapView)
         mapView.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
     }
     
+    private func move(to location: YMKPoint) {
+        mapView.mapWindow.map.move(
+            with: YMKCameraPosition(target: location, zoom: 17, azimuth: 0, tilt: 0),
+            animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 0.5),
+            cameraCallback: nil
+        )
+    }
 }
