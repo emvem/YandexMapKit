58126cdb80e0c016b7c93d214ba141c71f91ae8d
Vadim Em <mr.vadim134@gmail.com>
2025-05-21 11:16:10 +0500

    core data logic added

--- YandexMapKit/YandexMapKit/Services/CoreDataBookrmarkRepository.swift
+++ YandexMapKit/YandexMapKit/Services/CoreDataBookrmarkRepository.swift
@@ -6,15 +6,40 @@
 //
 
 import Foundation
+import CoreData
 
 class CoreDataBookrmarkRepository: BookmarkRepository {
-    let coreDataManager: CoreDataManager = CoreDataManager()
+    let coreDataManager: CoreDataManager = CoreDataManager.shared
     
     func saveBookmark(bookmark: Bookmark) {
-        
+        let context = coreDataManager.context
+        let mapObject = MapObject(context: context)
+        mapObject.title = bookmark.title
+        mapObject.subtitle = bookmark.subtitle
+        mapObject.id = UUID().uuidString
+        mapObject.lat = bookmark.latitude ?? 0.0
+        mapObject.lon = bookmark.longitude ?? 0.0
+
+        do {
+            try context.save()
+        } catch {
+            print("Save error: \(error)")
+        }
     }
     
     func getBookmarks() -> [Bookmark] {
+        let request: NSFetchRequest<MapObject> = MapObject.fetchRequest()
+        do {
+            let mapObjects = try coreDataManager.context.fetch(request)
+            return mapObjects.map {
+                Bookmark(title: $0.title,
+                         subtitle: $0.subtitle,
+                         latitude: $0.lat,
+                         longitude: $0.lon)
+            }
+        } catch {
+            print("Fetch error: \(error)")
+        }
         return []
     }
 }
